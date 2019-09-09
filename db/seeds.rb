99e125ba8e09c875bdb73eb9c7e40c4eb1fb2bdb
Mark Fusco <32991975+QuestionMark97@users.noreply.github.com>
2019-09-09 15:28:38 -0400

    Create User and Message models/migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: 'R2D2', password: 'test')
+User.create(username: 'Anakin', password: 'test')
+User.create(username: 'Luke', password: 'test')
+User.create(username: 'Padme', password: 'test')
+User.create(username: 'Palpatine', password: 'test')
+User.create(username: 'Yoda', password: 'test')
+User.create(username: 'Ben', password: 'test')
+User.create(username: 'Han', password: 'test')
+User.create(username: 'Leia', password: 'test')
+User.create(username: 'Lando', password: 'test')
