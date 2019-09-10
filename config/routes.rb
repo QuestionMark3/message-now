352662a063b3798ecabc61b9bfef766fc79bfa01
Mark Fusco <32991975+QuestionMark97@users.noreply.github.com>
2019-09-09 20:31:38 -0400

    Created UI functionality for creating messages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 	root 'chatroom#index'
 	get 'login' => 'sessions#new'
 	post 'login' => 'sessions#create'
-	delete 'logout'=> 'sessions#destroy'
+	delete 'logout' => 'sessions#destroy'
+	post 'message' => 'messages#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
