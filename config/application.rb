db02a0a50e786f534a870ba6539035c669abcbe9
Mark Fusco <32991975+QuestionMark97@users.noreply.github.com>
2019-09-25 12:27:02 -0400

    Replace coffeescript with javascript

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module MessageMe
 
     # config/application.rb
 		config.assets.initialize_on_precompile = false
+    config.generators.javascript_engine = :js
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
