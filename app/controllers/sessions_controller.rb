8588b83a7f1cb6f305c893439b4b57a316e1a6ff
Mark Fusco <32991975+QuestionMark97@users.noreply.github.com>
2019-09-29 23:20:28 -0400

    Create WebSocket authentication and replace sessions with cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 	def create
 			user = User.find_by(username: params[:session][:username])
 			if user && user.authenticate(params[:session][:password])
-				session[:user_id] = user.id
+				cookies.signed.encrypted[:user_id] = user.id
 				flash[:success] = 'You have successfully logged in'
 				redirect_to root_path
 			else
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
 	end
 
 	def destroy
-		session[:user_id] = nil
+		cookies.signed.encrypted[:user_id] = nil
 		flash[:success] = 'You have successfully logged out'
 		redirect_to login_path
 	end
