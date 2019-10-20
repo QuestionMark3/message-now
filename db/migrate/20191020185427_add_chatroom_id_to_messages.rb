926ab35f211c631ee4ded564db9521511c7beede
Mark Fusco <32991975+QuestionMark97@users.noreply.github.com>
2019-10-20 18:28:52 -0400

    Change name of migration to be accurate

--- db/migrate/20191020185427_add_chatroom_id_to_messages.rb
+++ db/migrate/20191020185427_add_chatroom_id_to_messages.rb
@@ -1,4 +1,4 @@
-class AddChatroomIdToUsers < ActiveRecord::Migration[5.2]
+class AddChatroomIdToMessages < ActiveRecord::Migration[5.2]
   def change
   	add_column :messages, :chatroom_id, :integer
   end
